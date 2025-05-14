75523466cbd223bc8ff1226bc15c794f3ad1f501
Rachel Huang <rachelhuang@Rachels-Laptop.local>
2025-05-14 14:17:13 -0400

    Updates

---
+++ precache-manifest.976f06ae223ef0373eacc734d20ff093.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e915ed897bbd4245cd09af516ce7b08",
+    "url": "/draft-test/index.html"
+  },
+  {
+    "revision": "3e8dba35ea1d755604f3",
+    "url": "/draft-test/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "ec0935851c1edf4cb708",
+    "url": "/draft-test/static/js/2.9e11b540.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/draft-test/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "3e8dba35ea1d755604f3",
+    "url": "/draft-test/static/js/main.fffdfd66.chunk.js"
+  },
+  {
+    "revision": "10784ee727f61f2d6b7f",
+    "url": "/draft-test/static/js/runtime~main.9bcd2ce6.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/draft-test/static/media/logo.06e73328.svg"
+  }
+]);
